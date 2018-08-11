b69798c1c4efb39199609176894f0677173c1bc4
arunabh29 <arunabh29@gmail.com>
2018-08-11 00:51:59 -0400

    Update multichoice_pipeline.groovy

--- multichoice_pipeline.groovy
+++ multichoice_pipeline.groovy
@@ -1,28 +1,40 @@
-agent any
-
-parameters {
-
-choice(name: ‘door_choice‘,
-choices: 'one\ntwo\nthree\nfour',
-description: ‘What door do you choose?‘)
-
-booleanParaln(name: 'CAN_DANCE',
-defauItVaIue: true.
-description: 'Checkbox parameter')
-
-string(name: ‘sTrAnGePaRaM‘ ,
-defaultValue: 'Dance! ' ,
-description: 'The funky chicken!')
-}
-
-
-stages {
+pipeline {
+  
+  agent any
+  
+  parameters {
+   
+   choice(name: ‘door_choice‘,
+   choices: 'one\ntwo\nthree\nfour',
+   description: ‘What door do you choose?‘)  
+  
+   booleanParam(name: 'CAN_DANCE',
+   defauItVaIue: true,
+   description: 'Checkbox parameter')
+   
+   string(name: ‘sTrAnGePaRaM‘ ,
+   defaultValue: 'Dance! ' ,
+   description: 'Do the funky chicken!')
+  
+  }
+  
+  
+  stages {
+  
+  
   stage('Examp1e') {
-    steps {
-      echo 'Hel'lo Worldl'
-      echo "Trying: $(params.door_choice)"
-      echo "We can dance: S(params.CAN_DANCE}"
-      echo "The DJ says: s(params.sTrAn6ePaRaM)"
-          }
-   }
-}
+  
+   steps {
+   
+      echo "Hello World !"
+      echo "Trying: ${params.door_choice}"
+      echo "We can dance: ${params.CAN_DANCE}"
+      echo "The DJ says: ${params.sTrAnGePaRaM}"
+         }
+  
+  
+	}
+   
+}  
+  
+   
